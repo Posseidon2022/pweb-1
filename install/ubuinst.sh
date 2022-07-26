5c9fd4c407b8dec587431cd126b6c1fca8f51ce0
@Posseidon2022 <98905208+Igorhenri0407@users.noreply.github.com>
2022-07-26 13:28:40 -0300

    Add files via upload

--- install/ubuinst.sh
+++ install/ubuinst.sh
@@ -74,14 +74,10 @@ function stop_install {
   [[ ! -e /bin/pweb ]]  && {
     title "INSTALAÇÃO CANCELADA"
     clear
-    cat /dev/null > ~/.bash_history && history -c
-    rm /bin/ubuinst* > /dev/null 2>&1
     exit;
  } || {
     title "INSTALAÇÃO CANCELADA"
     clear
-    cat /dev/null > ~/.bash_history && history -c
-    rm /bin/ubuinst* > /dev/null 2>&1
     exit;
 }
 }
@@ -96,7 +92,7 @@ function os_system {
   esac
 }
 function dependencias {
-  soft="python bc screen at nano unzip lsof netstat net-tools dos2unix nload jq python3 python-pip"
+  soft="python bc screen sshpass at nano unzip lsof netstat net-tools dos2unix nload jq curl figlet python3 python-pip cron"
    for i in $soft; do
     leng="${#i}"
     puntos=$((21 - $leng))
@@ -138,7 +134,7 @@ cat /dev/null > ~/.bash_history && history -c
 rm /bin/ubuinst* > /dev/null 2>&1
 exit;
 else
-  echo -e 'by: @Igorhenri0407' >/usr/lib/telegram
+  echo -e 'by: @nandoslayer' >/usr/lib/telegram
   msg -bar
   echo -e "\e[1;97m           \e[5m\033[1;100m   ATUALIZAÇÃO DO SISTEMA   \033[1;37m"
   msg -bar
@@ -154,8 +150,6 @@ else
   msg -bar
   apt install software-properties-common
   apt update -y
-  apt install figlet -y
-  apt install curl -y
   add-apt-repository ppa:ondrej/php -y
   apt update -y
   apt upgrade -y
@@ -215,7 +209,7 @@ function inst_base {
     echo -e "\n\033[1;36mINSTALANDO O APACHE2 \033[1;33mAGUARDE...\033[0m"
 apt install apache2 -y > /dev/null 2>&1
 apt install dirmngr apt-transport-https -y > /dev/null 2>&1
-apt install php8.1 libapache2-mod-php8.1 php8.1-xml php8.1-mcrypt php8.1-curl php8.1-mbstring php8.1-cli -y > /dev/null 2>&1
+apt install php7.4 libapache2-mod-php7.4 php7.4-xml php7.4-mcrypt php7.4-curl php7.4-mbstring php7.4-cli -y > /dev/null 2>&1
 systemctl restart apache2 > /dev/null 2>&1
 apt-get install mariadb-server -y > /dev/null 2>&1
 cd || exit
@@ -231,11 +225,11 @@ mysql -u root -p"$pwdroot" -e "GRANT ALL PRIVILEGES ON sshplus.* To 'root'@'loca
 mysql -u root -p"$pwdroot" -e "FLUSH PRIVILEGES" > /dev/null 2>&1
 echo '[mysqld]
 max_connections = 1000' >> /etc/mysql/my.cnf
-apt install php8.1-mysql -y > /dev/null 2>&1
+apt install php7.4-mysql -y > /dev/null 2>&1
 phpenmod mcrypt > /dev/null 2>&1
 systemctl restart apache2 > /dev/null 2>&1
 ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin > /dev/null 2>&1
-apt install php8.1-ssh2 -y > /dev/null 2>&1
+apt install php7.4-ssh2 -y > /dev/null 2>&1
 php -m | grep ssh2 > /dev/null 2>&1
 curl -sS https://getcomposer.org/installer | php > /dev/null 2>&1
 mv composer.phar /usr/local/bin/composer > /dev/null 2>&1
@@ -270,7 +264,7 @@ mv /tmp/pass /var/www/html/pages/system/pass.php
 
 }
 function inst_db { 
-sleep 1
+sleep 5
 if [[ -e "/var/www/html/bdgestorssh.sql" ]]; then
     mysql -h localhost -u root -p"$pwdroot" --default_character_set utf8 sshplus < /var/www/html/bdgestorssh.sql > /dev/null 2>&1
     rm /var/www/html/bdgestorssh.sql > /dev/null 2>&1
@@ -292,25 +286,36 @@ echo "
 @reboot /etc/autostart
 * * * * * /etc/autostart
 0 */12 * * * cd /var/www/html/pages/system/ && bash cron.backup.sh && cd /root
-5 */3 * * * cd /var/www/html/pages/system/ && /usr/bin/php cron.backup.php && cd /root
+5 */6 * * * cd /var/www/html/pages/system/ && /usr/bin/php cron.backup.php && cd /root
 * * * * * cd /var/www/html/pages/system/ && bash cron.userteste.sh && cd /root
-2 */3 * * * cd /var/www/html/pages/system/ && bash cron.autobackup.sh && cd /root
+2 */6 * * * cd /var/www/html/pages/system/ && bash cron.autobackup.sh && cd /root
 * * * * * /usr/bin/php /var/www/html/pages/system/cron.online.ssh.php
 @daily /usr/bin/php /var/www/html/pages/system/cron.rev.php
 * * * * * /usr/bin/php /var/www/html/pages/system/cron.ssh.php
 * * * * * /usr/bin/php /var/www/html/pages/system/cron.php
-*/30 * * * * /usr/bin/php /var/www/html/pages/system/hist.online.php" > cronset
+@monthly /usr/bin/php /var/www/html/pages/system/cron.limpeza.php" > cronset
 crontab cronset && rm cronset > /dev/null 2>&1
 }
-
+function fun_swap {
+			swapoff -a
+            rm -rf /bin/ram.img > /dev/null 2>&1
+            fallocate -l 4G /bin/ram.img > /dev/null 2>&1
+            chmod 600 /bin/ram.img > /dev/null 2>&1
+            mkswap /bin/ram.img > /dev/null 2>&1
+            swapon /bin/ram.img > /dev/null 2>&1
+            echo 50  > /proc/sys/vm/swappiness
+            echo '/bin/ram.img none swap sw 0 0' | tee -a /etc/fstab > /dev/null 2>&1
+            sleep 2
+}
 function tst_bkp {
 cd || exit
-sed -i "s;1020;$pwdroot;g" /var/www/html/lib/Database/Connection.php > /dev/null 2>&1
 sed -i "s;49875103u;$pwdroot;g" /var/www/html/pages/system/config.php > /dev/null 2>&1
 sed -i "s;localhost;$IP;g" /var/www/html/pages/system/config.php > /dev/null 2>&1
 }
 clear
 install_start
+install_continue
+install_continue2
 IP=$(wget -qO- ipv4.icanhazip.com)
 echo "America/Sao_Paulo" > /etc/timezone > /dev/null 2>&1
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
@@ -319,29 +324,8 @@ clear
 echo -e "\E[44;1;37m    INSTALANDO PAINEL    \E[0m"
 echo ""
 echo -e "WEB GESTOR-SSH" | figlet
-echo -e "                              \033[1;31mBy @Igorhenri0407\033[1;36m"
+echo -e "                              \033[1;31mBy @nandoslayer\033[1;36m"
 echo ""
-chave=$(curl -sSL "raw.githubusercontent.com/Igorhenri0407/pweb/painel/install/chave") &>/dev/null
-
-read -p "DIGITE A CHAVE DE INSTALAÇÃO: " key
-    
-         if [[ "$key" = "$chave" ]]
-          then
-               echo -e "[*] VALIDANDO A CHAVE DE INSTALAÇÃO"
-                sleep 2
-                echo $key > /bin/chave_inst
-                echo -e "[*] CHAVE ACEITA"
-                sleep 2
-            else
-            echo "[-] ESSA CHAVE NÃO É VÁLIDA!"
-            sleep 3
-            clear
-            cat /dev/null > ~/.bash_history && history -c
-            rm /bin/ubuinst* > /dev/null 2>&1
-            exit;
-          fi
-install_continue
-install_continue2
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
 	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
@@ -361,9 +345,6 @@ install_continue2
 	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 } > /dev/null
 echo ""
-echo -e "WEB GESTOR-SSH" | figlet
-echo -e "                              \033[1;31mBy @Igorhenri0407\033[1;36m"
-echo ""
 echo -e "\033[1;36mDEFINA UMA NOVA SENHA PARA\033[0m"
 echo -e "\033[1;36mO USUÁRIO ROOT DA VPS E\033[0m"
 echo -e "\033[1;36mPARA O USUÁRIO DO PHPMYADMIN!\033[0m"
@@ -381,10 +362,10 @@ cron_set
 fun_swap
 tst_bkp
 clear
-sed -i "s;upload_max_filesize = 2M;upload_max_filesize = 256M;g" /etc/php/8.1/apache2/php.ini > /dev/null 2>&1
-sed -i "s;post_max_size = 8M;post_max_size = 256M;g" /etc/php/8.1/apache2/php.ini > /dev/null 2>&1
+sed -i "s;upload_max_filesize = 2M;upload_max_filesize = 256M;g" /etc/php/7.4/apache2/php.ini > /dev/null 2>&1
+sed -i "s;post_max_size = 8M;post_max_size = 256M;g" /etc/php/7.4/apache2/php.ini > /dev/null 2>&1
 echo -e "PAINELWEB GESTOR-SSH" | figlet
-echo -e "                              \033[1;31mBy @Igorhenri0407\033[1;36m"
+echo -e "                              \033[1;31mBy @nandoslayer\033[1;36m"
 echo ""
 echo -e "\033[1;32mPAINEL INSTALADO COM SUCESSO!"
 echo ""
@@ -394,14 +375,12 @@ echo -e "\033[1;36m SENHA:\033[1;37m admin\033[0m"
 echo ""
 echo -e "\033[1;36m LOJA DE APPS:\033[1;37m http://$IP/apps\033[0m"
 echo ""
-echo -e "\033[1;36m PAGINA TESTE-FREE:\033[1;37m http://$IP/teste-free.php\033[0m"
-echo ""
 echo -e "\033[1;36m PHPMYADMIN:\033[1;37m http://$IP/phpmyadmin\033[0m"
 echo -e "\033[1;36m USUÁRIO:\033[1;37m root\033[0m"
 echo -e "\033[1;36m SENHA:\033[1;37m $pwdroot\033[0m"
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@Igorhenri0407\033[0m"
+echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@nandoslayer\033[0m"
 echo ""
 echo -ne "\n\033[1;31mENTER \033[1;33mpara retornar...\033[1;32m! \033[0m"; read
 systemctl restart apache2 > /dev/null 2>&1
